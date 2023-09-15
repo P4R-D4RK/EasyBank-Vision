65546a99bb6863d1cce357aa92286e5dcdc32f32
P4R-DARK <luis.282001@gmail.com>
2023-09-15 17:22:16 -0600

    Home y navbar terminado

--- EasyBank-VisionV1.0/src/app/app.component.ts
+++ EasyBank-VisionV1.0/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { User } from './interfaces/user-interface';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,12 @@ import { NavigationEnd, Router } from '@angular/router';
 export class AppComponent {
   title = 'EasyBank-Vision';
   activeNavBar: boolean = false;
-
+  user: User = {
+    first_name: '',
+    last_name: '',
+    user_number: '',
+    credit_card: '',
+  }
   constructor(private router: Router) {}
 
   ngOnInit(): void {
