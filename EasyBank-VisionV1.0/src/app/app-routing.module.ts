d6eb88dfc13db870d3c68a5c3dfe6aaf909795c7
Jorch34 <141645546+JorchGt34@users.noreply.github.com>
2023-09-15 18:35:19 -0600

    Update app-routing.module.ts

--- EasyBank-VisionV1.0/src/app/app-routing.module.ts
+++ EasyBank-VisionV1.0/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { SaldosComponent } from './components/saldos/saldos.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'saldos', component: SaldosComponent},
   { path: '**', redirectTo: 'login' },
 ];
 
