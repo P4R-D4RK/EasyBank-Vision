9a039eef298e47477a83c58e915463f83049324a
Jorch34 <141645546+JorchGt34@users.noreply.github.com>
2023-09-21 12:24:54 -0600

    Statement

--- EasyBank-VisionV1.0/src/app/app-routing.module.ts
+++ EasyBank-VisionV1.0/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { SaldosComponent } from './components/saldos/saldos.component';
+import { AccountStatementsComponent } from './components/account-statements/account-statements.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'saldos', component: SaldosComponent},
+  { path: 'statements', component: AccountStatementsComponent},
   { path: '**', redirectTo: 'login' },
 ];
 
